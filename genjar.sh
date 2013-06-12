ad3a5a1e3ebc69822bef3d268e8795f09ba20daf
laurent Andrey <laurent.andrey@loria.fr>
2013-06-12 18:12:09 +0200

    Clean up old view based on jgraph.. minor changes

--- genjar.sh
+++ genjar.sh
@@ -5,32 +5,36 @@ JAVAUTIL_NAME=javautils.jar
 CURDIR=`pwd`
 
 echo "We take stuff from $ANIMEXEC_TOP"
-echo "Clean stuff in $ANIMEXEC_TOP/bin (default package...)"
+echo "Clean stuff in $ANIMEXEC_TOP/bin (default package...). No class would stay at top level bin dir."
 ## NO RECURSE !
 \rm  -f $ANIMEXEC_TOP/bin/* 
 echo "***VERIFIER LES PROPERTIES ***"
+echo "Copying localized bundles files"
 
 cp "$ANIMEXEC_TOP/src/fr/loria/madynes/animjavaexec/MessagesBundle_en.properties"  "$ANIMEXEC_TOP/bin/fr/loria/madynes/animjavaexec/MessagesBundle_en.properties"
 echo "US messages bundle is the default (an unique for now !)"
 cp "$ANIMEXEC_TOP/src/fr/loria/madynes/animjavaexec/MessagesBundle_en.properties"  "$ANIMEXEC_TOP/bin/fr/loria/madynes/animjavaexec/MessagesBundle.properties"
-# No french messge for now
+# No french messages for now
 ##cp "$ANIMEXEC_TOP/src/simulip/gui/MessagesBundle_fr.properties"  "$ANIMEXEC_TOP/bin/simulip/gui/MessagesBundle_fr.properties"
 
 rm -f "$JAR_NAME"
 cd "$ANIMEXEC_TOP/bin"
-## jar is pain. A file could be produced using find...
+## jar is a pain. A file could be produced using find...
 jar cfe   "$CURDIR/$JAR_NAME"  fr.loria.madynes.animjavaexec.Main *
 
+echo "Adding javautils stuff"
 cd "$JAVAUTIL_TOP/bin"
 ## JavaUtils lib
 jar uf   "$CURDIR/$JAR_NAME" *
 
+
+echo "Building javautil jar"
 cd "$JAVAUTIL_TOP/bin"
 ###jar uf  "$CURDIR/$JAR_NAME" *
 jar cf "$CURDIR/$JAVAUTIL_NAME" *
 
-# One-jar lib for animexec
-cd "$CURDIR"
-jar tf    "$JAR_NAME"
-cd root; jar -cvfm ../animexe-onejar.jar boot-manifest.mf .
+# One-jar lib for animexec. Does not work. runanimjavaexec.sh
+#cd "$CURDIR"
+#jar tf    "$JAR_NAME"
+#cd root; jar -cvfm ../animexe-onejar.jar boot-manifest.mf .
 
--- runanimjavaexec.sh
+++ runanimjavaexec.sh
@@ -1,1 +1,9 @@
-java -cp .:/home/andreylocal/workspace/AnimJavaExec/bin:/home/andreylocal/workspace/JavaUtils/bin:/usr/local/jdk1.6.0_20/lib/tools.jar fr.loria.madynes.animjavaexec.Main
+TOOL_JAR=/usr/local/jdk1.6.0_20/lib/tools.jar
+TOOL_JAR=$JAVA_HOME/lib/tools.jar
+# AnimJavaExec with JavaUtils...
+ANIMJAVAEXEC_JAR=~/workspace/AnimJavaExec/animexe.jar
+
+# We can not use -jar option as we HAVE to set . in -cp for
+# the jvm spanw by jdi.
+
+java -cp .:$ANIMJAVAEXEC_JAR:$TOOL_JAR fr.loria.madynes.animjavaexec.Main $*
